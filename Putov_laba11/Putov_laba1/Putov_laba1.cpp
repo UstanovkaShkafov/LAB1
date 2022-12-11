4bba751077739963b112d33ee6e4fb69203a256a
UstanovkaShkafov <Sky.spawta@gmail.com>
2022-12-12 00:08:09 +0300

    lab 3 (change)

--- Putov_laba11/Putov_laba1/Putov_laba1.cpp
+++ Putov_laba11/Putov_laba1/Putov_laba1.cpp
@@ -8,6 +8,7 @@
 #include "check.h"
 #include "pipe.h"
 #include "cs.h"
+#include "graph.h"
 
 
 void menu() {
@@ -476,12 +477,14 @@ void SearchCS(unordered_map <int, CS>& cs_group)
 
 int main()
 {
+    System network;
+
     int option = -1;
     unordered_map<int, Pipe> YourPipes;
     unordered_map<int, CS> YourCS;
     while (option) {
         cout << "\nChoose option:\n 1.Add pipe 2.Add CS 3.View all objects " << "4.Edit pipe 5.Edit CS 6.Save 7.Load 8.Search pipe 9.Search CS \n10. Delete pipe 11. Delete cs \n 0.Exit\n";
-        option = getcorrectnumber(0, 11);
+        option = getcorrectnumber(0, 12);
         switch (option) {
         case 1:
         {
@@ -552,7 +555,47 @@ int main()
             DelCS(YourCS);
             menu();
             break;
+        case 12:
+        {
+            cout << "1.Connect 2.Disconnect" << endl;
+            int choise = getcorrectnumber(1, 2);
+            if (choise == 1) {
+                if ((network.cs_group.size() < 2) or (network.pipe_group.size() < 1))
+                    cout << "Not enough objects to create system" << endl;
+                else
+                    cin >> network;
+            }
+            else {
+                if (network.graphs.size() != 0) {
+                    cout << "Input the entering CS" << endl;
+                    int x = getcorrectnumber(0, CS::max_idstation);
+                    cout << "Input the exiting CS" << endl;
+                    int y = getcorrectnumber(0, CS::max_idstation);
+                    while (x == y) {
+                        cout << "You can't disconnect same CS" << endl;
+                        y = getcorrectnumber(0, CS::max_idstation);
+                    }
+                    auto i = network.graphs.cbegin();
+                    while (i != network.graphs.cend()) {
+                        if (((*i).second.id_entrance == x) and ((*i).second.id_exit == y)) {
+                            network.graphs.erase(i);
+                            break;
+                        }
+                        i++;
+                    }
+                }
+
+                else
+                    cout << "There is no any system" << endl;
+
+            }
+            for (auto& [i, j] : network.graphs)
+                cout << i << ") " << j.id_entrance << " " << j.id_exit << " " << j.id_pipe << endl;
+            break;
         }
+            break;
+        }
+        
 
        
     }
